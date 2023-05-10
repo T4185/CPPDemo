160c09c3d75d00bab4da15e820b7e8ccee30294c
T4185 <olivierwkboivin@hotmail.com>
2023-05-10 11:06:02 -0400

    Classe et ajustement ListView

---
+++ CPPDemo/Employes.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+using namespace System;
+
+public ref class Employes {
+public:
+	int ID;
+	String^ Nom;
+	String^ Prenom;
+	String^ NomUtilisateur;
+	String^ MotPasse;
+	String^ Admin;
+};
--- CPPDemo/mainForm.h
+++ CPPDemo/mainForm.h
@@ -41,6 +41,9 @@ namespace CPPDemo {
 	private: System::Windows::Forms::ColumnHeader^ columnHeader3;
 	private: System::Windows::Forms::ColumnHeader^ columnHeader1;
 	private: System::Windows::Forms::ColumnHeader^ columnHeader2;
+	private: System::Windows::Forms::ColumnHeader^ columnHeader4;
+	private: System::Windows::Forms::ColumnHeader^ columnHeader5;
+	private: System::Windows::Forms::ColumnHeader^ columnHeader6;
 	protected:
 
 	private:
@@ -57,17 +60,20 @@ namespace CPPDemo {
 		void InitializeComponent(void)
 		{
 			this->listView1 = (gcnew System::Windows::Forms::ListView());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->columnHeader3 = (gcnew System::Windows::Forms::ColumnHeader());
 			this->columnHeader1 = (gcnew System::Windows::Forms::ColumnHeader());
 			this->columnHeader2 = (gcnew System::Windows::Forms::ColumnHeader());
-			this->columnHeader3 = (gcnew System::Windows::Forms::ColumnHeader());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->columnHeader4 = (gcnew System::Windows::Forms::ColumnHeader());
+			this->columnHeader5 = (gcnew System::Windows::Forms::ColumnHeader());
+			this->columnHeader6 = (gcnew System::Windows::Forms::ColumnHeader());
 			this->SuspendLayout();
 			// 
 			// listView1
 			// 
-			this->listView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::ColumnHeader^  >(3) {
+			this->listView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::ColumnHeader^  >(6) {
 				this->columnHeader3, this->columnHeader1,
-					this->columnHeader2
+					this->columnHeader2, this->columnHeader4, this->columnHeader5, this->columnHeader6
 			});
 			this->listView1->FullRowSelect = true;
 			this->listView1->GridLines = true;
@@ -75,21 +81,15 @@ namespace CPPDemo {
 			this->listView1->Location = System::Drawing::Point(12, 12);
 			this->listView1->MultiSelect = false;
 			this->listView1->Name = L"listView1";
-			this->listView1->Size = System::Drawing::Size(279, 258);
+			this->listView1->Size = System::Drawing::Size(363, 258);
 			this->listView1->TabIndex = 0;
 			this->listView1->UseCompatibleStateImageBehavior = false;
 			this->listView1->View = System::Windows::Forms::View::Details;
 			this->listView1->SelectedIndexChanged += gcnew System::EventHandler(this, &mainForm::listView1_SelectedIndexChanged);
 			// 
-			// button1
+			// columnHeader3
 			// 
-			this->button1->Location = System::Drawing::Point(371, 79);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"Remplir";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &mainForm::button1_Click);
+			this->columnHeader3->Text = L"Id";
 			// 
 			// columnHeader1
 			// 
@@ -99,9 +99,27 @@ namespace CPPDemo {
 			// 
 			this->columnHeader2->Text = L"Prenom";
 			// 
-			// columnHeader3
+			// button1
 			// 
-			this->columnHeader3->Text = L"Id";
+			this->button1->Location = System::Drawing::Point(485, 113);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Remplir";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &mainForm::button1_Click);
+			// 
+			// columnHeader4
+			// 
+			this->columnHeader4->Text = L"NomUtilisateur";
+			// 
+			// columnHeader5
+			// 
+			this->columnHeader5->Text = L"MotPasse";
+			// 
+			// columnHeader6
+			// 
+			this->columnHeader6->Text = L"Admin";
 			// 
 			// mainForm
 			// 
