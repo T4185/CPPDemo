56aaacd57b8a0f22020720b13ac4858ce078264b
T4185 <olivierwkboivin@hotmail.com>
2023-05-10 10:59:20 -0400

    SQl connection + query

--- CPPDemo/mainForm.h
+++ CPPDemo/mainForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <exception>
 
 namespace CPPDemo {
 
@@ -8,6 +9,7 @@ namespace CPPDemo {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::Data::SqlClient;
 
 	/// <summary>
 	/// Summary for mainForm
@@ -35,6 +37,10 @@ namespace CPPDemo {
 			}
 		}
 	private: System::Windows::Forms::ListView^ listView1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ColumnHeader^ columnHeader3;
+	private: System::Windows::Forms::ColumnHeader^ columnHeader1;
+	private: System::Windows::Forms::ColumnHeader^ columnHeader2;
 	protected:
 
 	private:
@@ -51,22 +57,58 @@ namespace CPPDemo {
 		void InitializeComponent(void)
 		{
 			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->columnHeader1 = (gcnew System::Windows::Forms::ColumnHeader());
+			this->columnHeader2 = (gcnew System::Windows::Forms::ColumnHeader());
+			this->columnHeader3 = (gcnew System::Windows::Forms::ColumnHeader());
 			this->SuspendLayout();
 			// 
 			// listView1
 			// 
+			this->listView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::ColumnHeader^  >(3) {
+				this->columnHeader3, this->columnHeader1,
+					this->columnHeader2
+			});
+			this->listView1->FullRowSelect = true;
+			this->listView1->GridLines = true;
 			this->listView1->HideSelection = false;
 			this->listView1->Location = System::Drawing::Point(12, 12);
+			this->listView1->MultiSelect = false;
 			this->listView1->Name = L"listView1";
-			this->listView1->Size = System::Drawing::Size(239, 258);
+			this->listView1->Size = System::Drawing::Size(279, 258);
 			this->listView1->TabIndex = 0;
 			this->listView1->UseCompatibleStateImageBehavior = false;
+			this->listView1->View = System::Windows::Forms::View::Details;
+			this->listView1->SelectedIndexChanged += gcnew System::EventHandler(this, &mainForm::listView1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(371, 79);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Remplir";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &mainForm::button1_Click);
+			// 
+			// columnHeader1
+			// 
+			this->columnHeader1->Text = L"Nom";
+			// 
+			// columnHeader2
+			// 
+			this->columnHeader2->Text = L"Prenom";
+			// 
+			// columnHeader3
+			// 
+			this->columnHeader3->Text = L"Id";
 			// 
 			// mainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(464, 321);
+			this->ClientSize = System::Drawing::Size(645, 321);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->listView1);
 			this->Name = L"mainForm";
 			this->Text = L"mainForm";
@@ -74,5 +116,25 @@ namespace CPPDemo {
 
 		}
 #pragma endregion
+	private: System::Void listView1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try
+		{
+			String^ Cnstr = "Data Source=DESKTOP-1F1RIQT\\SQLEXPRESS01;Initial Catalog=Assurorama;Integrated Security=True";
+			SqlConnection sqlConn(Cnstr);
+			sqlConn.Open();
+			String^ query = "SELECT ID,Nom,Prenom FROM Employes";
+			SqlCommand cmd(query, % sqlConn);
+
+		}
+		catch (const std::exception& e)
+		{
+			MessageBox::Show("Failed");
+		}
+	
+	
+	}
 	};
 }
