adca778c03d5e569523039aefc297dd9b492ffed
T4185 <olivierwkboivin@hotmail.com>
2023-05-10 10:47:32 -0400

    Add project files.

---
+++ CPPDemo/mainForm.h
@@ -0,0 +1,78 @@
+#pragma once
+
+namespace CPPDemo {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for mainForm
+	/// </summary>
+	public ref class mainForm : public System::Windows::Forms::Form
+	{
+	public:
+		mainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~mainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::ListView^ listView1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->SuspendLayout();
+			// 
+			// listView1
+			// 
+			this->listView1->HideSelection = false;
+			this->listView1->Location = System::Drawing::Point(12, 12);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(239, 258);
+			this->listView1->TabIndex = 0;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			// 
+			// mainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(464, 321);
+			this->Controls->Add(this->listView1);
+			this->Name = L"mainForm";
+			this->Text = L"mainForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
